4b03fc0af173700937a3c7f6e0802d0955d0e802
mcgrady_525 <mcgrady_525@163.com>
2016-10-24 16:32:25 +0800

    完善Rights.Common项目。

---
+++ DEV/Rights/Rights.Common/Helper/DapperHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Rights.Common.Helper
+{
+    /// <summary>
+    /// Dapper帮助类
+    /// </summary>
+    public sealed partial class DapperHelper
+    {
+        /// <summary>
+        /// 创建数据库连接
+        /// </summary>
+        /// <returns></returns>
+        public static IDbConnection CreateConnection()
+        {
+            IDbConnection conn = new SqlConnection(ConfigHelper.GetConnectionString("ConnStr"));
+            return conn;
+        }
+    }
+}
---
+++ DEV/Rights/Rights.Common/Helper/RightsHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rights.Common.Helper
+{
+    /// <summary>
+    /// Rights帮助类
+    /// </summary>
+    public sealed partial class RightsHelper
+    {
+    }
+}
