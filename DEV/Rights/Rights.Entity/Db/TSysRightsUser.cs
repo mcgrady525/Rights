d45f1757299e09472b4f2258dab23a35920f0f8c
mcgrady_525 <mcgrady_525@163.com>
2016-10-25 17:46:25 +0800

    添加代码生成器生成的数据库实体；添加sql。

---
+++ DEV/Rights/Rights.Entity/Db/TSysRightsUser.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using Rights.Entity.Attributes;
+
+/// <summary>
+/// 
+/// </summary>
+namespace Rights.Entity.Db
+{
+	[Serializable]
+	[DataContract(IsReference = true)]
+	[Table("dbo.t_sys_rights_user")]
+	public partial class TSysRightsUser
+	{
+		public TSysRightsUser()
+		{
+			
+		}
+
+		/// <summary>
+		/// id
+		/// </summary>
+		[DataMember]
+		[Column("id", ColumnCategory=Category.IdentityKey)]
+		public int Id { get; set; }
+		
+		/// <summary>
+		/// 用户id
+		/// </summary>
+		[DataMember]
+		[Column("user_id")] 
+		public string UserId { get; set; }
+		
+		/// <summary>
+		/// 密码
+		/// </summary>
+		[DataMember]
+		[Column("password")] 
+		public string Password { get; set; }
+		
+		/// <summary>
+		/// 用户名
+		/// </summary>
+		[DataMember]
+		[Column("user_name")] 
+		public string UserName { get; set; }
+		
+		/// <summary>
+		/// 是否首次登陆改密
+		/// </summary>
+		[DataMember]
+		[Column("is_change_pwd")] 
+		public bool? IsChangePwd { get; set; }
+		
+		/// <summary>
+		/// 是否启用标识
+		/// </summary>
+		[DataMember]
+		[Column("enable_flag")] 
+		public bool? EnableFlag { get; set; }
+		
+		/// <summary>
+		/// 创建人id
+		/// </summary>
+		[DataMember]
+		[Column("created_by")] 
+		public int CreatedBy { get; set; }
+		
+		/// <summary>
+		/// 创建时间
+		/// </summary>
+		[DataMember]
+		[Column("created_time")] 
+		public DateTime CreatedTime { get; set; }
+		
+		/// <summary>
+		/// 最后修改人id
+		/// </summary>
+		[DataMember]
+		[Column("last_updated_by")] 
+		public int? LastUpdatedBy { get; set; }
+		
+		/// <summary>
+		/// 最后修改时间
+		/// </summary>
+		[DataMember]
+		[Column("last_updated_time")] 
+		public DateTime? LastUpdatedTime { get; set; }
+		
+	}
+}
