365384bbc7fa23bfa2232000b7a462a94b688cb1
mcgrady_525 <mcgrady_525@163.com>
2016-10-26 16:28:35 +0800

    finish组织机构Dao的增删改查。

--- DEV/Rights/Rights.Dao/Rights/RightsOrganizationDao.cs
+++ DEV/Rights/Rights.Dao/Rights/RightsOrganizationDao.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Rights.Entity.Db;
 using Rights.IDao.Rights;
+using Rights.Common.Helper;
+using Dapper;
+using Tracy.Frameworks.Common.Extends;
 
 namespace Rights.Dao.Rights
 {
@@ -19,7 +22,23 @@ namespace Rights.Dao.Rights
         /// <param name="item">待插入的记录</param>
         public void Insert(TRightsOrganization item)
         {
-            throw new NotImplementedException();
+            using (var conn= DapperHelper.CreateConnection())
+            {
+                var effectRows = conn.Execute(@"INSERT INTO dbo.t_rights_organization VALUES  ( @OrgName ,@ParentId ,@Code ,@OrganizationType ,@Sort ,@EnableFlag ,@CreatedBy ,@CreatedTime ,@LastUpdatedBy ,@LastUpdatedTime);", 
+                                new 
+                                {
+                                    @OrgName= item.Name,
+                                    @ParentId= item.ParentId,
+                                    @Code= item.Code,
+                                    @OrganizationType= item.OrganizationType,
+                                    @Sort= item.Sort,
+                                    @EnableFlag= item.EnableFlag,
+                                    @CreatedBy= item.CreatedBy,
+                                    @CreatedTime= item.CreatedTime,
+                                    @LastUpdatedBy= item.LastUpdatedBy,
+                                    @LastUpdatedTime= item.LastUpdatedTime
+                                });
+            }
         }
 
         /// <summary>
@@ -29,7 +48,18 @@ namespace Rights.Dao.Rights
         /// <returns></returns>
         public bool Update(TRightsOrganization item)
         {
-            throw new NotImplementedException();
+            var result = false;
+            using (var conn = DapperHelper.CreateConnection())
+            {
+                var effectRows = conn.Execute(@"UPDATE dbo.t_rights_organization SET name= @OrgName, parent_id= @ParentId, sort= @Sort WHERE id= @Id;",
+                                             new { @Id = item.Id, @OrgName = item.Name, @ParentId = item.ParentId, @Sort = item.Sort });
+                if (effectRows > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -39,7 +69,17 @@ namespace Rights.Dao.Rights
         /// <returns></returns>
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            var result = false;
+            using (var conn = DapperHelper.CreateConnection())
+            {
+                var effectRows = conn.Execute(@"DELETE FROM dbo.t_rights_organization WHERE id= @Id;", new { @Id = id });
+                if (effectRows > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -49,7 +89,18 @@ namespace Rights.Dao.Rights
         /// <returns></returns>
         public TRightsOrganization GetById(int id)
         {
-            throw new NotImplementedException();
+            TRightsOrganization result = null;
+            using (var conn = DapperHelper.CreateConnection())
+            {
+                var query = conn.Query<TRightsOrganization>(@"SELECT org.id AS Id, org.name AS NAME, org.parent_id AS ParentId, org.code AS Code, org.organization_type AS OrganizationType,
+                                                            org.sort AS Sort, org.enable_flag AS EnableFlag, org.created_by AS CreatedBy, org.created_time AS CreatedTime,
+                                                            org.last_updated_by AS LastUpdatedBy, org.last_updated_time AS LastUpdatedTime 
+                                                            FROM dbo.t_rights_organization AS org
+                                                            WHERE org.id= @Id", new { @Id = id });
+                result = query.FirstOrDefault();
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -58,7 +109,18 @@ namespace Rights.Dao.Rights
         /// <returns></returns>
         public List<TRightsOrganization> GetAll()
         {
-            throw new NotImplementedException();
+            List<TRightsOrganization> result = null;
+            using (var conn = DapperHelper.CreateConnection())
+            {
+                var query = conn.Query<TRightsOrganization>(@"SELECT org.id AS Id, org.name AS NAME, org.parent_id AS ParentId, org.code AS Code, org.organization_type AS OrganizationType,
+                                                            org.sort AS Sort, org.enable_flag AS EnableFlag, org.created_by AS CreatedBy, org.created_time AS CreatedTime,
+                                                            org.last_updated_by AS LastUpdatedBy, org.last_updated_time AS LastUpdatedTime 
+                                                            FROM dbo.t_rights_organization AS org
+                                                            ORDER BY org.code, org.sort;");
+                result = query.ToList();
+            }
+
+            return result;
         }
     }
 }
