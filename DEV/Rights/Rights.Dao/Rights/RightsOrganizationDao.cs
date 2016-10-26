8a21a0691c89f65c176ef36dd1ca3589fa667a64
mcgrady_525 <mcgrady_525@163.com>
2016-10-26 13:58:45 +0800

    add RightsOrganizationDao。

---
+++ DEV/Rights/Rights.Dao/Rights/RightsOrganizationDao.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rights.Entity.Db;
+using Rights.IDao.Rights;
+
+namespace Rights.Dao.Rights
+{
+    /// <summary>
+    /// 组织机构数据访问Dao
+    /// </summary>
+    public class RightsOrganizationDao : IRightsOrganizationDao
+    {
+        /// <summary>
+        /// 插入
+        /// </summary>
+        /// <param name="item">待插入的记录</param>
+        public void Insert(TRightsOrganization item)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="item">待更新的记录</param>
+        /// <returns></returns>
+        public bool Update(TRightsOrganization item)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id">待删除记录的id</param>
+        /// <returns></returns>
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 依id查询
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns></returns>
+        public TRightsOrganization GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 获取所有记录
+        /// </summary>
+        /// <returns></returns>
+        public List<TRightsOrganization> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DEV/Rights/Rights.IDao/Rights/IRightsOrganizationDao.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rights.Entity.Db;
+
+namespace Rights.IDao.Rights
+{
+    /// <summary>
+    /// 组织机构数据访问Dao接口
+    /// </summary>
+    public interface IRightsOrganizationDao
+    {
+        /// <summary>
+        /// 插入
+        /// </summary>
+        /// <param name="item">待插入的记录</param>
+        void Insert(TRightsOrganization item);
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="item">待更新的记录</param>
+        /// <returns></returns>
+        bool Update(TRightsOrganization item);
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id">待删除记录的id</param>
+        /// <returns></returns>
+        bool Delete(int id);
+
+        /// <summary>
+        /// 依id查询
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns></returns>
+        TRightsOrganization GetById(int id);
+
+        /// <summary>
+        /// 获取所有记录
+        /// </summary>
+        /// <returns></returns>
+        List<TRightsOrganization> GetAll();
+
+
+
+    }
+}
